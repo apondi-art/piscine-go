4b4c759562c64862c7753dc820b3dcca3903b109
apondi <aiyanarich64@gmail.com>
2024-05-22 12:45:14 +0300

    feat: added function that takes two arguments and match the first one with the second one

---
+++ umatch/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	str1 := os.Args[1]
+	str2 := os.Args[2]
+	i := 0
+	for _, char := range str2 {
+		if char == rune(str1[i]) {
+			i++
+		}
+	}
+	if (i) == len(str1) {
+		fmt.Println(str1)
+	}else{
+		return
+	}
+}
